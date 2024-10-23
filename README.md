7cb23b6b7c23ea22d94e0429a6453e58c592c2a3
lucasvsantanna <santannavlucas@gmail.com>
2024-10-23 09:46:19 -0300

    Remove logo

--- README.md
+++
@@ -1,4 +0,0 @@
-Web application challenge Video: https://youtu.be/2gz2qaheM5s
-
-I would improve the API to return more complete objects. 
-For exemple: the responsabilities field and type of equipment.
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,10 @@
-import { companiesApi } from '@/services/companies/companies-api'
-import { TractianLogo } from './logo-tractian'
 import { NavbarButtons } from './navbar-buttons'
 import { Suspense } from 'react'
 
 export async function Navbar() {
   return (
     <nav className="bg-dark-blue w-full h-14 flex justify-between items-center px-4">
-      <TractianLogo />
+      <div />
 
       <Suspense fallback={<div className='text-white z-10 bg-white'>Loading...</div>}>
         <NavbarButtons />
